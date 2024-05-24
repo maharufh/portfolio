61e5d75f24dcd78539cb14d9781230989d15cfe1
Maharufh Ali Mir <85804873+maharufh@users.noreply.github.com>
2024-05-24 21:21:12 +0530

    Update Achievement.js

--- src/components/resume/Achievement.js
+++ src/components/resume/Achievement.js
@@ -11,53 +11,53 @@ const Achievement = () => {
     >
       <div>
         <div className="py-6 lgl:py-12 font-titleFont flex flex-col gap-4">
-          <p className="text-sm text-designColor tracking-[4px]">2007 - 2010</p>
+          <p className="text-sm text-designColor tracking-[4px]">2021 - 2024</p>
           <h2 className="text-3xl md:text-4xl font-bold">Company Experience</h2>
         </div>
         <div className="mt-6 lgl:mt-14 w-full h-[1000px] border-l-[6px] border-l-black border-opacity-30 flex flex-col gap-10">
           <ResumeCard
-            title="Lorem ipsum dolor sit amet."
-            subTitle="Lorem ipsum dolor sit amet alternative."
-            result="Success"
-            des="Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora itaque accusamus corporis dolor debitis natus sapiente, voluptates possimus minima totam!"
-          />
-          <ResumeCard
-            title="Lorem ipsum dolor sit amet."
-            subTitle="Lorem ipsum dolor sit amet alternative."
-            result="Success"
-            des="Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora itaque accusamus corporis dolor debitis natus sapiente, voluptates possimus minima totam!"
-          />
-          <ResumeCard
-            title="Lorem ipsum dolor sit amet."
-            subTitle="Lorem ipsum dolor sit amet alternative."
-            result="Success"
-            des="Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora itaque accusamus corporis dolor debitis natus sapiente, voluptates possimus minima totam!"
+          title="FrontEnd Developer"
+          subTitle="Web Design and Development (September'23-November'23)"
+          result="Kolkata"
+          des=" I Worked there as a frontend Developer where I had to manage everything regarding frontend including design the website."
+        />
+        <ResumeCard
+          title="Frntend Engineer"
+          subTitle="Frontend Developer Team - (April'22-June'22)"
+          result="Delhi"
+          des="It's Travel management website where I mainly worked in frontend design "
+        />
+        <ResumeCard
+          title="Full Stack Developer"
+          subTitle="Mern Developer (2021-2022)"
+          result="WFO"
+          des="It's full stack website regarding college information and carrier counselling"
           />
         </div>
       </div>
       <div>
         <div className="py-6 lgl:py-12 font-titleFont flex flex-col gap-4">
-          <p className="text-sm text-designColor tracking-[4px]">2007 - 2010</p>
+          <p className="text-sm text-designColor tracking-[4px]">2021 - 2024</p>
           <h2 className="text-3xl md:text-4xl font-bold">Job Experience</h2>
         </div>
         <div className="mt-6 lgl:mt-14 w-full h-[1000px] border-l-[6px] border-l-black border-opacity-30 flex flex-col gap-10">
           <ResumeCard
-            title="Lorem ipsum dolor sit amet."
-            subTitle="Lorem ipsum dolor sit amet alternative."
-            result="Success"
-            des="Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora itaque accusamus corporis dolor debitis natus sapiente, voluptates possimus minima totam!"
+            title="FrontEnd Developer"
+            subTitle="Web Design and Development (September'23-November'23)"
+            result="Kolkata"
+            des=" I Worked there as a frontend Developer where I had to manage everything regarding frontend including design the website."
           />
           <ResumeCard
-            title="Lorem ipsum dolor sit amet."
-            subTitle="Lorem ipsum dolor sit amet alternative."
-            result="Success"
-            des="Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora itaque accusamus corporis dolor debitis natus sapiente, voluptates possimus minima totam!"
+            title="Frntend Engineer"
+            subTitle="Frontend Developer Team - (April'22-June'22)"
+            result="Delhi"
+            des="It's Travel management website where I mainly worked in frontend design "
           />
           <ResumeCard
-            title="Lorem ipsum dolor sit amet."
-            subTitle="Lorem ipsum dolor sit amet alternative."
-            result="Success"
-            des="Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora itaque accusamus corporis dolor debitis natus sapiente, voluptates possimus minima totam!"
+            title="Full Stack Developer"
+            subTitle="Mern Developer (2021-2022)"
+            result="WFO"
+            des="It's full stack website regarding college information and carrier counselling specially after 12th"
           />
         </div>
       </div>
